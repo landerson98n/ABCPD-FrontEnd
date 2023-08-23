385c02ea6dbf04338f82f8455144048718d6bf69
landerson98n <landerson98n@gmail.com>
2023-08-23 17:29:05 -0300

    update

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,12 +7,12 @@ export function Header(){
     return (
     <Container>
         <div style={{paddingLeft:'4vw',display:'flex', justifyContent:'center', alignItems:'center'}}>
-            <div style={{position:'relative', width:'4vw', height:'3vw'}}>
-                <Image src={logo} alt="Logo" layout='fill' objectFit='contain' />
+            <div style={{ width:'4vw', height:'3vw'}}>
+                <Image src={logo} alt="Logo" layout='responsive' objectFit='contain' />
             </div>
 
-            <div style={{position:'relative', width:'12vw', height:'6vw'}}>
-                <Image src={logo2} alt="Logo" layout='fill' objectFit='contain' />
+            <div style={{ width:'12vw', height:'4vw'}}>
+                <Image src={logo2} alt="Logo" layout='responsive' objectFit='contain' />
             </div>
         </div>
            
