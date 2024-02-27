a74fc4d695a44846e09d1f388e759613bc7717b0
landerson98n <landerson98n@gmail.com>
2024-02-27 00:19:09 -0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,35 @@
 /* eslint-disable no-undef */
-import React from "react";
+"use client";
 import StyledComponentsRegistry from "./registry";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {AlertContextProvider} from "@/context/AlertContextProvider";
 import {AnimalContextProvider} from "@/context/AnimalContextProvider";
 
-export const metadata = {
-    "title": "ABCPD",
-    "description": "Pagina ABCPD"
-};
-
 const queryClient = new QueryClient();
 
-const RootLayoutProviders: React.FC = ({children}) => <AnimalContextProvider>
-    <AlertContextProvider>
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    </AlertContextProvider>
-</AnimalContextProvider>;
 
-const RootLayout: React.FC = ({children}) => <StyledComponentsRegistry>
-    <html lang="pt-br">
-        <head>
-            <title>Registro</title>
-        </head>
-        <body style={{"margin": 0,
-            "overflowX": "hidden"}}>{children}</body>
-    </html>
-</StyledComponentsRegistry>;
+export default function RootLayout ({
+    children
+}: {
+  children: React.ReactNode
+}) {
 
-const RootLayoutWithProviders: React.FC = ({children}) => <RootLayoutProviders>
-    <RootLayout>{children}</RootLayout>
-</RootLayoutProviders>;
-export default RootLayoutWithProviders;
+    return (
+        <AnimalContextProvider>
+            <AlertContextProvider>
+                <QueryClientProvider client={queryClient}>
+                    <StyledComponentsRegistry>
+                        <html lang="pt-br">
+                            <head>
+                                <title>Registro</title>
+                            </head>
+                            <body style={{"margin": 0,
+                                "overflowX": "hidden"}}>{children}</body>
+                        </html>
+                    </StyledComponentsRegistry>
+                </QueryClientProvider>
+            </AlertContextProvider>
+        </AnimalContextProvider>
+    );
 
+}
