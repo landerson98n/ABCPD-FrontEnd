44e47c00d6f993f999394cde79357ad3168ada73
landerson98n <landerson98n@gmail.com>
2024-03-05 17:17:41 -0300

    image update

--- assets/index.ts
+++ assets/index.ts
@@ -1,60 +1,61 @@
-import gado from './Foto_Gado_Central.svg'
-import logo from './Logo.svg'
-import logo2 from './Logo2.svg'
-import fundoLogin from './fundoLogin.svg'
-import touros from './touros.svg'
-import fundo2 from './Fundo 2.svg'
-import vaca from './vaca.svg'
-import logoBranca from './LogoBranca.svg'
-import logo2Branca from './Logo2Branca.svg'
-import social from './social-prove.svg'
-import cartao from './cartao.svg'
-import pix from './pix.svg'
-import card from './card.svg'
-import boleto from './boleto.png'
-import Home from './Home.svg'
-import animal from './animal.svg'
-import comunic from './comunic.svg'
-import user from './user.svg'
-import done from './done.svg'
-import waiting from './waiting.svg'
-import arrowLeft from './Shape.svg'
-import hamb from './hamburguer.svg'
-import searchIcon from './search.svg'
-import seta from './seta.svg'
-import boi from './boiExample.svg'
-import add from './add.svg'
-import group from './group.svg'
-import animalBlue from './animalBlue.svg'
-import logoCert from './logo_cert.svg'
+/* eslint-disable no-duplicate-imports */
+import gado from "./boi2.jpg";
+import logo from "./Logo.svg";
+import logo2 from "./Logo2.svg";
+import fundoLogin from "./fundoLogin.svg";
+import touros from "./boi.jpg";
+import fundo2 from "./Fundo 2.svg";
+import vaca from "./boi.jpg";
+import logoBranca from "./LogoBranca.svg";
+import logo2Branca from "./Logo2Branca.svg";
+import social from "./social-prove.svg";
+import cartao from "./cartao.svg";
+import pix from "./pix.svg";
+import card from "./card.svg";
+import boleto from "./boleto.png";
+import Home from "./Home.svg";
+import animal from "./animal.svg";
+import comunic from "./comunic.svg";
+import user from "./user.svg";
+import done from "./done.svg";
+import waiting from "./waiting.svg";
+import arrowLeft from "./Shape.svg";
+import hamb from "./hamburguer.svg";
+import searchIcon from "./search.svg";
+import seta from "./seta.svg";
+import boi from "./boiExample.svg";
+import add from "./add.svg";
+import group from "./group.svg";
+import animalBlue from "./animalBlue.svg";
+import logoCert from "./logo_cert.svg";
 export {
-  logoCert,
-  group,
-  add,
-  boi,
-  seta,
-  searchIcon,
-  hamb,
-  arrowLeft,
-  done,
-  waiting,
-  user,
-  Home,
-  animal,
-  comunic,
-  boleto,
-  card,
-  pix,
-  cartao,
-  social,
-  gado,
-  logo,
-  logo2,
-  fundoLogin,
-  touros,
-  fundo2,
-  vaca,
-  logoBranca,
-  logo2Branca,
-  animalBlue,
-}
+    logoCert,
+    group,
+    add,
+    boi,
+    seta,
+    searchIcon,
+    hamb,
+    arrowLeft,
+    done,
+    waiting,
+    user,
+    Home,
+    animal,
+    comunic,
+    boleto,
+    card,
+    pix,
+    cartao,
+    social,
+    gado,
+    logo,
+    logo2,
+    fundoLogin,
+    touros,
+    fundo2,
+    vaca,
+    logoBranca,
+    logo2Branca,
+    animalBlue
+};
